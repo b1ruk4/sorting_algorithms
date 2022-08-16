4c9fcb39b3e68fb3cba3d527953db54951e0c35f
b1ruk4 <birukwondimubkk@gmail.com>
2022-08-16 14:33:10 -0700

    fix

--- sort.h
+++ sort.h
@@ -21,6 +21,7 @@ struct listint_s *prev;
 struct listint_s *next;
 } listint_t;
 
+void quick_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
