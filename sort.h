9d3819869ae38595d077a7d69fc1902e06be7e94
b1ruk4 <birukwondimubkk@gmail.com>
2022-08-16 14:45:36 -0700

    task 5

--- sort.h
+++ sort.h
@@ -21,6 +21,7 @@ struct listint_s *prev;
 struct listint_s *next;
 } listint_t;
 
+void cocktail_sort_list(listint_t **list);
 void quick_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void print_array(const int *array, size_t size);
