a3c0056fdc88d0f162491d7d071eb10122af3503
b1ruk4 <birukwondimubkk@gmail.com>
2022-08-16 15:04:30 -0700

    task 6

--- sort.h
+++ sort.h
@@ -21,6 +21,7 @@ struct listint_s *prev;
 struct listint_s *next;
 } listint_t;
 
+void counting_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void quick_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
